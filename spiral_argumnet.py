4dcc0325bc65f5984c7115de70da0ff294f0d1fe
maruf-dev <maruf123erkinov@gmail.com>
2021-08-15 21:56:18 +0300

    speral

---
+++ spiral_argumnet.py
@@ -0,0 +1,10 @@
+import turtle
+
+def spiral (sides, turn, color, width):
+    t = turtle.Turtle()
+    t.color(color)
+    t.width(width)
+    for n in range(sides):
+        t.forward(n)
+        t.right(turn)
+spiral(150, -30, "cyan", 5)         
