df45964eb008490faf353af6e1e4b881d7a2fa88
maruf-dev <maruf123erkinov@gmail.com>
2021-09-11 18:20:40 +0500

    chechs if it starts with "K" or not

---
+++ starts_K.py
@@ -0,0 +1,12 @@
+def start_K(s):
+    if s[0] == "K":
+       return True
+    else:
+         return False
+
+"""def start_K(s):
+    return s[0] == "K"""
+
+
+print(start_K("Kelly"))
+print(start_K("Abe"))
