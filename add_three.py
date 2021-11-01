611a621af70231daf6442c4d03fba102dddd8fe9
maruf-dev <maruf123erkinov@gmail.com>
2021-11-01 17:34:59 +0500

    addes 3 input

---
+++ add_three.py
@@ -0,0 +1,6 @@
+a = input("enter a number : ")
+b = input("enter another number :")
+c = input("enter third number : ")
+sum = int(a) + int(b) + int(c)         
+         
+print(f"{a} + {b} + {c}  = {sum}")
